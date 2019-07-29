fcd562a1aace5a6500400a766f34b07f5cde5198
vThushy <40529679+vThushy@users.noreply.github.com>
2019-07-29 22:35:09 +0530

    After created node modules and notes.

--- ExpressJS/BasicCRUD/app.js
+++ ExpressJS/BasicCRUD/app.js
@@ -1,9 +1,63 @@
 const exp = require('express');
 const employeeApp = exp();
+const employee = require('./employees');
+
+//to get parse body values
+employeeApp.use(exp.json());
+employeeApp.use(exp.urlencoded({ extended: true }));
 
 employeeApp.listen(5000, () => console.log("Server Up...."));
 
+employeeApp.get('/', (req, res) => {
+   res.send("<h1>Employee Application</h1>")
+})
 
- employeeApp.get('/api/employees', (request, response) => {
-    response.send("Server Work on localohost");
- });
+employeeApp.get('/employee', (req, res) =>
+   res.json(employee)
+);
+
+employeeApp.get('/employee/:id', (req, res) => {
+   var empId = req.params.id;
+   res.json(employee.filter(value => value.id == empId));
+});
+
+employeeApp.post('/employee', (req, res) => {
+   const newEmployee = {
+      "id": req.body.id,
+      "name": req.body.name,
+      "destination": req.body.destination
+   }
+
+   employee.push(newEmployee);
+   res.json(employee);
+});
+
+employeeApp.put('/employee', (req, res) => {
+   const updateEmployee = {
+      "id": req.body.id,
+      "name": req.body.name,
+      "destination": req.body.destination
+   }
+
+   employee.forEach(emp => {
+      if (emp.id == updateEmployee.id) {
+         emp.name = updateEmployee.name;
+         emp.destination = updateEmployee.destination;
+      } else {
+         res.send("employee not found!");
+      }
+   })
+   res.json(employee);
+})
+
+employeeApp.delete('/employee/:id', (req, res) => {
+   deleteEmployeeId = req.params.id;
+   employee.forEach(emp => {
+      if (emp.id == deleteEmployeeId.id) {
+         employee.pop(emp.id -1);
+      } else {
+         res.send("Employee not found..")
+      }
+   })
+   res.json(employee);
+})
